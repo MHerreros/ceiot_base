f72fa9b649a0ebb65ca8ae5c05695959aec0faf9
Matias <matiasherreros97@gmail.com>
2023-10-12 05:05:06 +0000

    Added DB env variables

--- api/infra/db/postgre.database.config.js
+++ api/infra/db/postgre.database.config.js
@@ -1,15 +1,12 @@
 const Pool = require('pg').Pool;
-
-// FINISH IMPLEMENTATION OF CONFIG MODULE
-// LOOK INTO CONFIG LOADING AFTER POOL (MAKING ENV VARIABLES UNABAILABLE WHEN THE DB CONNECTION STARTS)
 const config = require('../../config');
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'api_db',
-  password: 'Ceiot2023',
-  port: 5432,
+  user: config.POSTGRES_USER,
+  host: config.POSTGRES_HOST,
+  database: config.POSTGRES_DATABASE,
+  password: config.POSTGRES_PASSWORD,
+  port: config.POSTGRES_PORT,
 });
 
 module.exports = pool;
